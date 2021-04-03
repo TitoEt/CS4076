11d6a658f757bfba3af231375f0ee8e1be588dea
TitoEt <72010582+TitoEt@users.noreply.github.com>
2021-04-03 23:08:39 +0100

    Update rooms.h

--- rooms.h
+++ rooms.h
@@ -1,11 +1,33 @@
-#ifndef ROOMS_H
-#define ROOMS_H
+#ifndef ROOM_H_
+#define ROOM_H_
+
+#include <map>
+#include <string>
+#include <vector>
+#include "clues.h"
+using namespace std;
+using std::vector;//sequence container -- think pass the parcel but with pointers to an array element
+
+class Room {
+
+private:
+	string description;
+	map<string, Room*> exits;
+	string exitString();
+    vector <Clue> cluesInRoom;
 
 
-class rooms
-{
 public:
-    rooms();
+    int numberOfItems();
+	Room(string description);
+	void setExits(Room *north, Room *east, Room *south, Room *west);
+	string shortDescription();
+	string longDescription();
+	Room* nextRoom(string direction);
+    void addClue(Clue *inClue);
+    string displayClue();
+    int isClueInRoom(string inString);
+    void removeClueFromRoom(int location);
 };
 
-#endif // ROOMS_H
+#endif
