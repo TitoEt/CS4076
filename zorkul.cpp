7f73d1b5fabe5065fc374f1528e13469c6f83e7d
TitoEt <72010582+TitoEt@users.noreply.github.com>
2021-04-13 21:18:39 +0100

    Update zorkul.cpp

--- zorkul.cpp
+++ zorkul.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 #include "zorkul.h"
 #include "clues.h"
+#include "room.h"
 
 ZorkUL::ZorkUL() {
     createRooms();
@@ -11,19 +12,31 @@ ZorkUL::ZorkUL() {
 
 void ZorkUL::createRooms()  {
 
-    Room *one, *two, *three,*four,*five,*six;
-        one = new Room("Outside","You stand outside the crimescene. Go east to enter the house.");
-        two = new Room("Living Room","Inside the living room, there are two bodies. I should look for clues.");
-        three = new Room("Bathroom","There's someone in here. What are they doing?");
-        four = new Room("Kitchen","There's blood on the floor.");
-        five = new Room("Garden","flowers again");
-        six = new Room("Flower Shop", "Flowers");
+    Room *one, *two, *three, *four, *five, *six;
+
+        one = new Room("Outside; You stand outside the crimescene. Go east to enter the house.");
+        two = new Room("Living Room; Inside the living room, there are two bodies. I should look for clues.");
+        three = new Room("Bathroom; There's someone in here. What are they doing?");
+        four = new Room("Kitchen; There's blood on the floor.");
+        five = new Room("Garden; flowers again");
+        six = new Room("Flower Shop; Flowers");
+
         roomList.push_back(one);
         roomList.push_back(two);
         roomList.push_back(three);
         roomList.push_back(four);
         roomList.push_back(five);
         roomList.push_back(six);
+
+    clues *bloodstains, *pillbottle, *baseballbat, *fingerprint, *gelsium;
+
+        two->addClues(bloodstains);
+        three->addClues(pillbottle);
+        four->addClues(baseballbat);
+        four->addClues(fingerprint);
+        five->addClues(gelsium);
+        six->addClues(gelsium);
+        six->addClues(fingerprint);
 
 
 //             (N, E, S, W)
@@ -38,15 +51,6 @@ void ZorkUL::createRooms()  {
 
 Room* ZorkUL::getRoom() {
     return currentRoom;
-}
-
-void ZorkUL::createClues(){
-    clues *pillbottle = new clue("Pill Bottle", true);
-    clues *bloodstain = new Clue("Blood Stains", true);
-    clues *gelsiumflower = new Clue("Gelsium", true);
-    clues *fingerprint = new Clue("Culprit finger print", true);
-    clues *bat = new clues("Baseball bat", true);
-    clues *autopsy = new Clue("Autopsy Results", true);
 }
 
 string ZorkUL::play() {
