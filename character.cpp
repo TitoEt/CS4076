378b64e119644a0ce140e1924b3d01f19e267b33
TitoEt <72010582+TitoEt@users.noreply.github.com>
2021-04-17 21:12:34 +0100

    Update character.cpp

--- character.cpp
+++ character.cpp
@@ -18,3 +18,15 @@ string Character::longDescription()
     ret += "\t"+ (*i).getLongDescription() + "\n";
   return ret;
 }
+
+//for the inventory, not too sure how well this would work so im uploading it commented out just so you can see!
+/*
+string Character::shortDescription(){
+    string clues = "Inventory: \n";
+
+    for(unsigned int i = 0; i < cluesInCharacter.size(); i++){
+        clues += "" + cluesInCharacter[i].getShortDescription() + "\n";
+    }
+    return clues;
+}
+*/
