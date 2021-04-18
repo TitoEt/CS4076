9706ee5f28b3626619e08378c3d7b78e8793ac3a
TitoEt <72010582+TitoEt@users.noreply.github.com>
2021-04-19 00:28:08 +0100

    Add files via upload

---
+++ suspect.cpp
@@ -0,0 +1,10 @@
+#include "suspect.h"
+#include <iostream>
+
+suspect::suspect(int a, int b, int c, int d)
+{
+   int suspecta(a);
+   int suspectb(b);
+   int suspectc(c);
+   int suspectd(d);
+}
---
+++ suspect.h
@@ -0,0 +1,12 @@
+#ifndef SUSPECT_H
+#define SUSPECT_H
+
+
+class suspect
+{
+public:
+    int suspecta, suspectb, suspectc, suspectd;
+    suspect(int a, int b, int c, int d);
+};
+
+#endif // SUSPECT_H
