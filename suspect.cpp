ad918508aa9be936fed57e0d50aab605979cbe59
TitoEt <72010582+TitoEt@users.noreply.github.com>
2021-04-19 03:24:26 +0100

    Update suspect.cpp

--- suspect.cpp
+++ suspect.cpp
@@ -15,3 +15,13 @@ T Large(T n1, T n2)
 {
     return (n1 > n2) ? n1 : n2;
 }
+
+struct S{
+    unsigned int b : 3;
+};
+
+ S s= {6};
+    ++s.b;
+    std::cout << s.b << '\n';
+    ++s.b;
+    std::cout << s.b << '\n';
