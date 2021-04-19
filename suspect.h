d023730b4977a86801ad0456eee90e5f7e29f31d
TitoEt <72010582+TitoEt@users.noreply.github.com>
2021-04-19 01:22:38 +0100

    Update suspect.h

--- suspect.h
+++ suspect.h
@@ -6,7 +6,15 @@ class suspect
 {
 public:
     int suspecta, suspectb, suspectc, suspectd;
-    suspect(int a, int b, int c, int d);
+    suspect(int a, int b, int c, int d) {suspecta = a; suspectb = b; suspectc = c; suspectd = d;};
+
+    suspect(const suspect &suspect) {suspecta = suspect.suspecta; suspectb = suspect.suspectb; suspectc = suspect.suspectc; suspectd = suspect.suspectd;}
+
+    int getSusA()    { return suspecta;}
+    int getSusB()    { return suspectb;}
+    int getSusC()    { return suspectc;}
+    int getSusD()    { return suspectd;}
+
 };
 
 #endif // SUSPECT_H
