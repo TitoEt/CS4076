aca36f6ee64d6fbbb93ebc75a3ff73dd93c24ece
TitoEt <72010582+TitoEt@users.noreply.github.com>
2021-04-19 01:23:42 +0100

    Update character.cpp

--- character.cpp
+++ character.cpp
@@ -10,3 +10,13 @@ void Character::addClues(clues *clue) {
     inventory.push_back(*clue);
     delete clue;
 }
+
+bool Character::cluesCollected(){
+    int collection = 0;
+
+    if(collection >= 0){
+        return true;
+    }else{
+        return false;
+    }
+}
